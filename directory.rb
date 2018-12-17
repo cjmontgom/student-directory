073812842fe71ffc9465f80461e6642f0dff4222
Chloe <chloemontgomery01@gmail.com>
2018-12-17 15:19:44 +0000

    Extract code into methods.

--- directory.rb
+++ directory.rb
@@ -13,10 +13,23 @@ students = [
   "Norman Bates"
 ]
 # and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
 # finally, we print the total using the count method to total the elements in the array
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#now we've extracted code into methods, the body reads like English
+print_header
+print(students)
+print_footer(students)
