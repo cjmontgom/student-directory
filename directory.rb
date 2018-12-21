57a275c2df2e527afb850c9b7015fda4bd64e108
Chloe <chloemontgomery01@gmail.com>
2018-12-21 11:00:18 +0000

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,18 +1,19 @@
 def input_students
-  puts "Please enter the names of the students, followed by the students hobby, and country of birth"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name of the student"
   students = []
-  name = gets.chomp
+  name = gets.delete("\n")
   while !name.empty? do
-    hobby = gets.chomp
-    country_of_birth = gets.chomp
-    students << {name: name, cohort: :november, hobby: hobby, country: country_of_birth}
+    puts "Please enter student's cohort"
+    cohort = gets.chomp
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students."
+    puts "Please enter the name of the next student"
+    puts "To finish, just hit return again"
     name = gets.chomp
   end
-  students.each do |data|
-    if data[:hobby].empty?
-      data[:hobby] << "Unknown"
+  students.each do |student|
+    if student[:cohort].empty?
+      student[:cohort] << "Unknown"
     end
   end
  students
@@ -24,19 +25,55 @@ def print_header
 end
 
 def display(students)
-  students.each_with_index do |student, index|
-    print "#{index + 1}. #{student[:name]}, (#{student[:cohort]} cohort)".ljust(40)
-    print " Hobby: #{student[:hobby]}".ljust(40)
-    puts " Birthplace: #{student[:country]}"
+  if students.length >= 1
+    def pick_cohort
+      puts "Which cohort would you like to view?"
+      cohort = gets.chomp
+      if cohort.empty?
+        pick_cohort
+      end
+      cohort
+    end
+    cohort = pick_cohort
+    students.each_with_index do |student, index|
+      if student[:cohort] == cohort
+        print "#{index + 1}. #{student[:name]}"
+        puts "(#{student[:cohort]} cohort)".rjust(60 - student[:name].length)
+      end
+    end
+  else
+    puts "List empty."
   end
 end
 
 # finally, we print the total using the count method to total the elements in the array
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.length == 1
+    puts "Overall, we have 1 great student"
+  else puts "Overall, we have #{students.count} great students"
+  end
 end
 
-students = input_students
-print_header
-display(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      display(students)
+      print_footer(students)
+    when "9"
+      exit #terminate program
+    else
+      puts "Not recognised. Try again."
+    end
+  end
+end
+
+interactive_menu
